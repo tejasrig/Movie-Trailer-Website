22bcf8a12a9fa47ede16afbad2e73c08b3276f68
tejasrig <tejasrig@gmail.com>
2017-11-10 01:33:35 -0800

    FirstCommit

--- media.py
+++ media.py
@@ -1,9 +1,14 @@
 import webbrowser
+
+
+# Class to hold Movie instances
 class Movie():
-    def __init__(self,mov_title,mov_storyline,poster_image,trailer_yt):
+    """This class holds the attributes needed for a movie"""
+    def __init__(self, mov_title, mov_storyline, poster_image, trailer_yt):
         self.title = mov_title
         self.storyline = mov_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_yt
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
